87899e80b9cfe6dddc57b1ba6630c2659c065a2d
EyepatchDoge <45701926+EyepatchDoge@users.noreply.github.com>
2025-01-21 17:11:38 -0500

    commit_13
    created a system for turning off and turning back on a bubble after using it

--- GGJ2025/Assets/Scripts/Kasper/Bubble.cs
+++ GGJ2025/Assets/Scripts/Kasper/Bubble.cs
@@ -1,13 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
-
+    #region variables
     [SerializeField]
     FuelSystem fuelSystem;
 
     [SerializeField]
     float fuelAmount = 25f;
+    [SerializeField]
+    float hidetimer = 2.0f;
+    #endregion
 
     void OnTriggerEnter(Collider other)
     {
@@ -17,6 +21,8 @@ public class Bubble : MonoBehaviour
         {
             Debug.Log("a bubble!");
             fuelSystem.AddFuel(fuelAmount);
+
+            StartCoroutine(HideAndShow(hidetimer, other.gameObject));
         }
 
         else
@@ -24,7 +30,12 @@ public class Bubble : MonoBehaviour
             Debug.Log("something's broken");
         }
     }
-
+    private IEnumerator HideAndShow(float delay, GameObject bubble)
+    {
+        bubble.SetActive(false);
+        yield return new WaitForSeconds(delay); // Wait for the specified time
+        bubble.SetActive(true); // Reactivate the bubble GameObject after the delay
+    }
 
 
 }
